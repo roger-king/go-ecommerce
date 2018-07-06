5fdfe352cd1609542c73d202d78ff820b365a45b
Roger King <rogr.king@gmail.com>
2018-07-06 15:37:09 -0400

    adding db connection

--- main.go
+++ main.go
@@ -1,21 +1,34 @@
-package src
+package main
 
 import (
 	"log"
 	"net/http"
 	"os"
 	"github.com/gorilla/handlers"
+	"github.com/rogr-king/go-ecommerce/app"
 	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
 )
 
+var engine *xorm.Engine
+
 func main() {
+	var err error
+
+	engine, err = xorm.NewEngine("mysql", "localhost/devdb?charset=utf8")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT is not defined")
 	}
 
-	router := NewRouter()
+	router := app.NewRouter()
 
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})

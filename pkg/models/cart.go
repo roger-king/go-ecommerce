df25983541b7e6b7416673781ec022741aa4d1d4
Roger King <rogr.king@gmail.com>
2018-07-20 19:06:02 -0400

    feat: create cart

--- pkg/models/cart.go
+++ pkg/models/cart.go
@@ -8,3 +8,19 @@ type Cart struct {
 	User     User      `gorm:"foreignkey:UserID;association_foreignkey:Refer"`
 	Products []Product `gorm:"many2many:carts_products;"`
 }
+
+func createCart(cart *Cart) (*Cart, error) {
+	var err error
+	err = db.Create(&cart).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	createdCart := Cart{
+		User:     cart.User,
+		Products: cart.Products,
+	}
+
+	return &createdCart, nil
+}
